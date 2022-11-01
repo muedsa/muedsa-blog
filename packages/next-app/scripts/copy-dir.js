f3762afe2669240916bc05af35fae38a85165e8b
MUEDSA <7676275+muedsa@users.noreply.github.com>
2022-11-01 17:51:06 +0800

    update: copy public

--- packages/next-app/scripts/copy-dir.js
+++ packages/next-app/scripts/copy-dir.js
@@ -1,12 +1,14 @@
 import fs from 'fs-extra';
 
-const source = '.next';
-const destination = '../../.next';
+function copyDir(source, destination) {
+  fs.copy(source, destination, function (err) {
+    if (err){
+      console.log('An error occured while copying the folder.')
+      return console.error(err)
+    }
+    console.log('Copy completed!')
+  });
+}
 
-fs.copy(source, destination, function (err) {
-  if (err){
-    console.log('An error occured while copying the folder.')
-    return console.error(err)
-  }
-  console.log('Copy completed!')
-});
+copyDir('.next', '../../.next');
+copyDir('public', '../../public');
